0863f1521ecd29f75805e9bcf7fd357fc0099eed
Sumaih_Alhothifi <sahmadalhothifi@stu.kau.edu.sa>
2024-05-11 21:06:40 +0300

    Final Commit :))))))))

--- fetch_services.php
+++ fetch_services.php
@@ -1,15 +1,109 @@
 <?php
 session_start();
 include 'connection.php';
+
+$loggedIn = isset($_SESSION["user_id"]);
+if (!$loggedIn) {
+    header("Location: Login.html");
+    exit;
+  }
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html dir="rtl" lang="ar">
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Wedding Planner</title>
+<title>الحزم المخصصة</title>
 <style>
+    header {
+    width: 100%;
+    top: 0;
+    left: 0;
+    z-index: 10;
+    padding: 0 10px;
+}
+
+.navbar .hamburger-btn {
+    display: none;
+    color: #000000;
+    cursor: pointer;
+    font-size: 1.5rem;
+}
+
+.navbar .logo {
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    margin-left: auto;
+}
+
+.navbar .logo img {
+    width: 70px;
+    /* Adjust the width as needed */
+    height: auto;
+    margin-right: 10px;
+}
+
+.navbar .logo h2 {
+    font-family: 'Arial', sans-serif;
+    color: #000000;
+    font-weight: 600;
+    font-size: 2rem;
+}
+
+.navbar .links {
+    display: flex;
+    gap: 35px;
+    list-style: none;
+    align-items: center;
+}
+
+.navbar .close-btn {
+    position: absolute;
+    right: 20px;
+    top: 20px;
+    display: none;
+    color: #000;
+    cursor: pointer;
+}
+
+.navbar .links a {
+    color: #000000;
+    font-size: 1.1rem;
+    font-weight: 500;
+    text-decoration: none;
+    transition: 0.1s ease;
+}
+
+.navbar .links a:hover {
+    color: #000000;
+}
+
+.dropdown-menu {
+    display: none;
+    position: absolute;
+    background-color: #5755555f;
+    min-width: 160px;
+    box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+    z-index: 1;
+}
+
+.dropdown-menu a {
+    color: #333333;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+}
+
+.dropdown-menu a:hover {
+    background-color: #9b59b6;
+}
+
+.nav-item.dropdown:hover .dropdown-menu {
+    display: block;
+}
+
 body {
      
 background-image: url("images/bg.jpg");
@@ -93,10 +187,182 @@ button {
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
     text-align: center;
 }
+ /* Footer CSS */
+ footer {
+  background-color: #33333344;
+  color: #fff;
+  text-align: center;
+  padding: 10px;
+  position: relative;
+  bottom: 0;
+  width: 100%;
+  margin-top: 500px;
+}
+
+.footer__container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.footer__links {
+  width: 100%;
+  max-width: 1000px;
+  display: flex;
+  justify-content: center;
+}
+
+.footer__link--wrapper {
+  display: flex;
+}
+
+.footer__link--items {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin: 16px;
+  text-align: left;
+  width: 160px;
+  box-sizing: border-box;
+}
+
+.footer__link--items h2 {
+  margin-bottom: 16px;
+  color: #000000;
+}
+
+.footer__link--items a {
+  color: #000000;
+  text-decoration: none;
+  margin-bottom: 0.5rem;
+  transition: 0.3 ease-out;
+}
+
+.footer__link--items a:hover {
+  color: #000000;
+  transition: 0.3 ease-out;
+}
+
+#footer__logo {
+  color: #000000;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 2rem;
+}
+#footer__logo {
+  margin-bottom: 2rem;
+}
+.footer__logo img {
+  width: 50px; /* Adjust the width to your desired size */
+  height: auto; /* Maintain aspect ratio */
+  vertical-align: middle; /* Align with the text vertically */
+}
+
+.footer__text {
+  display: inline-block; /* Allow the text to be inline with the logo */
+  vertical-align: middle; /* Align with the logo vertically */
+  margin-left: 10px; /* Adjust the margin as needed */
+  font-size: 20px; /* Adjust the font size to your desired size */
+  font-weight: bold; /* Apply desired font weight */
+}
+.social__media {
+  max-width: 1000px;
+  width: 100%;
+  margin-top: -60px;
+}
+.social__media--wrap {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 90%;
+  max-width: 1000px;
+  margin: 40px auto 0 auto;
+}
+.social__icons {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 240px;
+}
+
+.fab {
+  font-family: "Font Awesome 5 Brands";
+  color: black;
+}
 
 </style>
 </head>
 <body>
+<body>
+<!-- Navbar Section -->
+<header>
+  <nav class="navbar">
+    <span class="hamburger-btn material-symbols-rounded">menu</span>
+    <a href="index.php" class="logo">
+      <img src="images/SorourIcon.png" alt="logo">
+      <h2>سُرور</h2>
+    </a>
+    <ul class="links">
+      <span class="close-btn material-symbols-rounded">close</span>
+      <li>
+        <a class="nav-link" href="about.php">عن سُرور</a>
+      </li>
+      <li>
+        <a class="nav-link" href="service.php">الخدمات</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          أدوات التخطيط
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="busplitFINAL.php">تخطيط الميزانية</a>
+          <a class="dropdown-item" href="guest.php">إدارة قائمة الضوف</a>
+          <a class="dropdown-item" href="checklist.php">إدارة المهام</a>
+          <a class="dropdown-item" href="Vendor.php">الخدمة مقدم</a>
+        </div>
+      </li>
+      <li>
+        <a class="nav-link" href="cart.php">السلة</a>
+      </li>
+      <li>
+        <a class="nav-link" href="Login.html">تسجبل الدخول</a>
+      </li>
+      <li>
+        <a class="nav-link" href="SignUp.html">إنشاء حساب</a>
+      </li>
+
+      <?php if ($loggedIn) { ?>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            حسابي
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="userProfile.php">الاعدادات</a>
+            <a class="dropdown-item" href="LogOut.php">تسجيل الخروج</a>
+          </div>
+        </li>
+      <?php } else if ($loggedInV) { ?>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            حسابي
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="vendorProfile.php">الاعدادات</a>
+            <a class="dropdown-item" href="LogOut.php">تسجيل الخروج</a>
+          </div>
+        </li>
+      <?php } ?>
+
+      <li>
+        <a class="nav-link" href="SignUp_vendor.html">هل انت بائع؟</a>
+      </li>
+    </ul>
+  </nav>
+</header>
+  <!-- end header inner -->
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Retrieve the budgets for venue, catering, and musician
@@ -186,7 +452,7 @@ document.addEventListener("DOMContentLoaded", function() {
 <br>
 <br>
 <br>
-<h1 style="color:#896ba6;">This is where you'll discover packages customized to your budget</h1>
+<h1 style="color:#896ba6;">هذا هو المكان الذي ستكتشف فيه الحزم المخصصة لميزانيتك</h1>
 
 <div class="service-container">
     <!-- Your PHP-generated service HTML here -->
@@ -200,6 +466,57 @@ document.addEventListener("DOMContentLoaded", function() {
     }
     ?>
 </div>
-
+    <!-- Footer Section -->
+    <footer>
+      <div class="footer__container">
+        <div class="footer__links">
+          <div class="footer__link--wrapper">
+            <div class="footer__link--items">
+              <h2>عنا</h2>
+              <a href="">الاعدادات</a>
+              <a href="about.php">المزيد</a>
+            </div>
+            <div class="footer__link--items">
+              <h2>تواصل معنا</h2>
+              <a href="/">راسلنا </a>
+              <a href="/">الدعم</a>
+            </div>
+          </div>
+          <div class="footer__link--wrapper">
+            <div class="footer__link--items">
+              <h2>سجل معنا</h2>
+              <a href="SignUp.html">زائر جديد؟</a>
+              <a href="SignUp_vendor.html">صاحب عمل؟</a>
+            </div>
+          </div>
+        </div>
+        <section class="social__media">
+          <div class="social__media--wrap">
+        <div class="footer__logo">
+          <a href="index.php" id="footer__logo">
+            <img src="images/SorourIcon.png" alt="sorour Logo"><span class="footer__text">سُرور</span>
+          </a>
+        </div>
+      <p class="website__rights">© جميع الحقوق محفوظة. فريق سُرور</p>
+      <div class="social__icons">
+        <a href="/" class="social__icon--link" target="_blank"
+          ><i class="fab fa-facebook"></i
+        ></a>
+        <a href="/" class="social__icon--link"
+          ><i class="fab fa-instagram"></i
+        ></a>
+        <a href="/" class="social__icon--link"
+          ><i class="fab fa-youtube"></i
+        ></a>
+        <a href="/" class="social__icon--link"
+          ><i class="fab fa-linkedin"></i
+        ></a>
+        <a href="/" class="social__icon--link"
+          ><i class="fab fa-twitter"></i
+        ></a>
+      </div>
+      </div>
+      </section>
+  </footer>
 </body>
 </html>
